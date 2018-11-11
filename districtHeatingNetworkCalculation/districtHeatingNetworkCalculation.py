8c167ae087cf4c2b4fe09c3143f2f33366570f3f
Rosemann <toni.rosemann@tu-dresden.de>
2018-11-11 22:41:50 +0100

    refactoring less functions

--- districtHeatingNetworkCalculation/districtHeatingNetworkCalculation.py
+++ districtHeatingNetworkCalculation/districtHeatingNetworkCalculation.py
@@ -5,35 +5,35 @@ import matplotlib.pyplot as plt
 
 #calculation System 2
 
-def pressurePump1(m):
-    a2 = -1
-    a1 = 0.5
-    a0 = 12
+def pressurePump(m, a):
+    a2 = a[0]
+    a1 = a[1]
+    a0 = a[2]
 
     dp = a2 * m**2 + a1 * m + a0
     return dp
 
-def pressureLoss1(m):
-    b1 = 0.05
-
-    dp = b1 * m**2
+def pressureLoss(m, b):
+    dp = b * m**2
     return dp
 
-def pressureLoss2(m):
-    b2 = 0.08
-
-    dp = b2 * m**2
-    return dp
 
 def equationSystem2(z):
     m0      = z[0]
     m1      = z[1]
     m2      = z[2]
 
+    Pu1     = empty(3)
+    Pu1[0]  = -1
+    Pu1[1]  = 0.5
+    Pu1[2]  = 12
+    Loss1   = 0.05
+    Loss2   = 0.08
+
     F       = empty(3)
-    F[0]     = pressurePump1(m0) + pressureLoss1(m1)
-    F[1]     = pressureLoss2(m2) - pressureLoss1(m1)
-    F[2]     = m0 - m1 - m2
+    F[0]    = pressurePump(m0, Pu1) + pressureLoss(m1, Loss1)
+    F[1]    = pressureLoss(m2, Loss2) - pressureLoss(m1, Loss1)
+    F[2]    = m0 - m1 - m2
 
     return F
 
