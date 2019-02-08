acebcf6c58690645690923be52acaf3936df469c
raketenschnitzel <toni.rosemann@posteo.de>
2019-02-08 09:11:28 +0100

    Implemented a more pythonic modernFibonacci2 version and a list version

--- tests/generatorsAndIterators.py
+++ tests/generatorsAndIterators.py
@@ -8,9 +8,10 @@ def generatorfunction(size):
     for i in range(size):
         yield i    
         
-def modernFibonacci(order):
+def modernFibonacci1(order):
     """
     Modern fibonacci starts with 0, 1 fort the first two elements instead of 1, 1
+    generator implementation
     """
     last1 = 1
     last2 = 0
@@ -18,7 +19,29 @@ def modernFibonacci(order):
         temp  = last1
         last1 = last1 + last2
         last2 = temp
-        yield last1        
+        yield last1 
+        
+def modernFibonacci2(order):
+    """
+    More pythonic version of modernFibonacci1.
+    This way we omit the temp variable
+    """
+    last1, last2 = 1, 0
+    for i in range(order):
+        last1, last2 = last1 + last2, last1
+        yield last1          
+        
+def fibonacci2(n):
+    """
+    list implementation
+    more ressources needed
+    """
+    a = b = 1
+    result = []
+    for i in range(n):
+        result.append(a)
+        a, b = b, a + b
+    return result    
 
 class iterator:
     def __next__():
@@ -35,9 +58,16 @@ if __name__ == '__main__':
     
     "Test Fibonacci"
     order = 1
-    print(f'Print fibonacci {order:>02}: {list(modernFibonacci(order))}')
+    print(f'Print modernFibonacci1 {order:>02}: {list(modernFibonacci1(order))}')
     order = 3
-    print(f'Print fibonacci {order:>02}: {list(modernFibonacci(order))}')
+    print(f'Print modernFibonacci1 {order:>02}: {list(modernFibonacci1(order))}')
     order = 10
-    print(f'Print fibonacci {order:>02}: {list(modernFibonacci(order))}')
+    print(f'Print modernFibonacci1 {order:>02}: {list(modernFibonacci1(order))}')
+    
+    order = 1
+    print(f'Print modernFibonacci2 {order:>02}: {list(modernFibonacci2(order))}')
+    order = 3
+    print(f'Print modernFibonacci2 {order:>02}: {list(modernFibonacci2(order))}')
+    order = 10
+    print(f'Print modernFibonacci2 {order:>02}: {list(modernFibonacci2(order))}')
     
