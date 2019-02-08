9f4f3bba7853a900e29858203d11781dddeb253f
raketenschnitzel <toni.rosemann@posteo.de>
2019-02-08 08:54:27 +0100

    Implemented simple generator function and fibonacci as generator function

--- tests/generatorsAndIterators.py
+++ tests/generatorsAndIterators.py
@@ -4,17 +4,40 @@
     Here we test iterators and generators
 '''
 
-def myrange(size):
+def generatorfunction(size):
     for i in range(size):
-        yield i     
+        yield i    
+        
+def modernFibonacci(order):
+    """
+    Modern fibonacci starts with 0, 1 fort the first two elements instead of 1, 1
+    """
+    last1 = 1
+    last2 = 0
+    for i in range(order):
+        temp  = last1
+        last1 = last1 + last2
+        last2 = temp
+        yield last1        
 
 class iterator:
     def __next__():
-        pass
+        pass       
+        
+if __name__ == '__main__':     
+    "First we test a generator function"
+    agenerator = generatorfunction(5)
+    print(agenerator)
+    print(f'A generator function actually is an object: {type(agenerator)}')
+    print(f'It\s also iterable: {int(agenerator.__next__())}')
+    print(f'It\s also iterable: {int(agenerator.__next__())}')
+    print(f'Feed it to a list to get the list:{list(agenerator.__iter__())}')
     
-def main():
-    alist = myrange(10)
-    print(list(alist))
-        
-        
-if __name__ == '__main__': main()
+    "Test Fibonacci"
+    order = 1
+    print(f'Print fibonacci {order:>02}: {list(modernFibonacci(order))}')
+    order = 3
+    print(f'Print fibonacci {order:>02}: {list(modernFibonacci(order))}')
+    order = 10
+    print(f'Print fibonacci {order:>02}: {list(modernFibonacci(order))}')
+    
