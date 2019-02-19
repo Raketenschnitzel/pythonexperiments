ada8dcf5b072044a8a1e1934d1af79f52ed584fa
Rosemann <toni.rosemann@tu-dresden.de>
2019-02-20 00:32:21 +0100

    Still a bug

--- exercises/classExercise01.py
+++ exercises/classExercise01.py
@@ -5,57 +5,53 @@
 
 
 class Converter(object):
-  
-  
+
+
   def __init__(self):
-    self.conv = {
-      'I' : 1,
-      'V' : 5,
-      'X' : 10,
-      'L' : 50,
-      'C' : 100,
-      'D' : 500,
-      'M' : 1000
-    }
-    
-    self._representation = None
-    
-    print(self.conv)
-    
-  def reduceOrder(self):
-    print('Reduce Order')
-    pass  
-  
-  def representation(self, roman):
-    if roman is None:
-      return self._representation
-    else:
-      self._representation = roman
-      
-  def convertPart(self, number):
-    for k,v in self.conv.items():
-      if v == number:
-        self.representation(k)
-    
+    self.nums = [1, 5, 10, 50, 100, 500, 1000]
+    self.romes = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+    self._representation = ''
+    print(self.nums)
+    print(self.romes)
+
+
+  def convertPart(self, amount):
+    result = ''
+    sign = self.romes.pop()
+    for i in range(amount):
+        result += sign
+    print('convertPart:{}'.format(result))
+    return result
+
+
   def convertFull(self,number):
     # find char for highest mod
-    max = max(self.conv.values)
-    print('Max of conv:{}'.format(max))
-    
+    maxval = self.nums.pop()
+    # print('Maxval:{}'.format(maxval))
+
     # find part to replace
-    replace = number//max
-    rest = number//max
-    
-    # leave recursion
+    replace = number//maxval
+    rest = number%maxval
+    # print('self.representation:{}'.format(self._representation))
+    # return or recursion
     if rest == 0 and replace > 0:
-      self.convertPart(replace)
-      return self
+        return self.convertPart(replace)
+    elif rest > 0 and replace == 0:
+        self.romes.pop()
+        return self.convertFull(rest)
     elif rest > 0 and replace > 0:
-      self.convertPart(replace)
-      
-    
+        self._representation += self.convertPart(replace)
+        self._representation += self.convertFull(rest)
+
     #subtractive notation
-    return romanString
-    
+    return self._representation
+
 if __name__ == '__main__':
-  aclass = Converter()
+  aClass = Converter()
+  print('Ergebnis Test 8: {}'.format(aClass.convertFull(8)))
+  aClass = Converter()
+  print('Ergebnis Test 9: {}'.format(aClass.convertFull(9)))
+  aClass = Converter()
+  print('Ergebnis Test 56: {}'.format(aClass.convertFull(56)))
+  aClass = Converter()
+  print('Ergebnis Test 803: {}'.format(aClass.convertFull(803)))
