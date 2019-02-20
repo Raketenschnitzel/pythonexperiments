c0e72a5ca61e41463c48b526b8e0a979fc180cf6
Toni Rosemann <Toni.Rosemann@training.futuretrainings.com>
2019-02-20 10:17:18 +0100

    Not working: Tried to implement negative notation

--- exercises/classExercise01.py
+++ exercises/classExercise01.py
@@ -8,37 +8,51 @@ class Converter(object):
 
 
   def __init__(self):
-    self.nums = [1, 5, 10, 50, 100, 500, 1000]
-    self.romes = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+    self.nums = (1, 5, 10, 50, 100, 500, 1000)
+    self.romes = ('I', 'V', 'X', 'L', 'C', 'D', 'M')
+    self.numsstack = list(self.nums)
+    self.romesstack = list(self.romes)
     self._representation = ''
-    print(self.nums)
-    print(self.romes)
+    print(self.numsstack)
+    print(self.romesstack)
 
 
   def popHighestValue(self):
-    return self.nums.pop(), self.romes.pop()
+    if len(self.numsstack) > 0 and len(self.romesstack) > 0:
+        return self.numsstack.pop(), self.romesstack.pop()
+    else:
+        return None
 
 
   def convertPart(self, amount, sign):
     result = ''
-    for i in range(amount):
-        result += sign
-    print('convertPart:{}'.format(result))
+    if amount < 4:
+        for i in range(amount):
+            result += sign
+    print('convertPart({},{}):{}'.format(amount,sign,result))
     return result
 
 
   def convertFull(self,number):
     # find char for highest mod
     maxval, sign = self.popHighestValue()
-    replace = number//maxval
-    rest = number%maxval
-    # print('self.representation:{}'.format(self._representation))
+    try:
+        replace = number//maxval
+        rest = number%maxval
+    except:
+        print('Corrupt floor division {}//{}'.format(number, maxval))
+
     # return or recursion
     if rest == 0 and replace > 0:
         self._representation += self.convertPart(replace, sign)
         return
     elif rest > 0 and replace == 0:
-        self.convertFull(rest)
+        if rest > (maxval - self.numsstack[-1]):
+            self._representation += self.romesstack[-2] + sign
+            self.popHighestValue()
+            self.convertFull(rest - maxval)
+        else:
+            self.convertFull(rest)
     elif rest > 0 and replace > 0:
         self._representation += self.convertPart(replace, sign)
         self.convertFull(rest)
